0dbd7a233ef5c88bd010911e99f117a8ac018f21
gerichilli <63441133+gerichilli@users.noreply.github.com>
2021-02-13 00:04:44 +0700

    update

--- scripts.js
+++ scripts.js
@@ -150,3 +150,89 @@ const data = [
       "tools": ["React", "Sass"]
     }
   ]
+
+let dataClone = [...data].reverse();
+let filteredTag = [];
+
+const itemWrapper = document.querySelector('.wrapper');
+const filterWrapper = document.querySelector('.filter');
+const filteredTagWrapper = document.querySelector('.filtered');
+
+creatItemHtmlContent();
+
+// Hide filtered when not choosing tag filtered
+filterWrapper.classList.remove('filter--active');
+
+// Tag click behavior
+const allItemTagWrapper = document.querySelectorAll('.item__tag-wrapper');
+
+allItemTagWrapper.forEach((item) => {
+  item.addEventListener('click', function(e){
+    if(e.target.classList.contains('item__tag')) {
+      // console.log(e.target.innerHTML);
+      const tag = e.target.innerHTML;
+      if(filteredTag.indexOf(tag) === -1) {
+        filteredTag.push(tag);
+        creatFilteredTagHtml(tag);
+      }
+    filterWrapper.classList.add('filter--active');
+    } 
+  })
+})
+
+function creatItemHtmlContent() {
+  // Creating Tag list 
+    dataClone.forEach((item) => {
+      let tagListHTML = '';
+
+      item.languages?.forEach((lang)=> {
+        tagListHTML += `<div class="item__tag tag" data-languages="${lang}">${lang}</div>`;
+      });
+
+      item.tools?.forEach((tool) => {
+        tagListHTML += `<div class="item__tag tag" data-tools="${tool}">${tool}</div>`;
+      })
+    // Push them to html
+    itemWrapper.insertAdjacentHTML('afterbegin', `
+      <div class="item ${item.featured ? 'featured' : ''}">
+          <div class="item__logo">
+            <img src="${item.logo}" alt="">
+          </div>
+          <div class="item__content">
+            <div class="item__content--1">
+              <div class="item__company text--cyan">${item.company}</div>
+              ${item.new ? '<div class="item__new">New!</div>' : ''}
+              ${item.featured ? '<div class="item__featured">Featured</div>' : ''}
+            </div>
+            <div class="item__content--2">
+              <div class="item__position"><a href="#">${item.position}</a></div>
+            </div>
+            <div class="item__content--3">
+              <div class="item__postedat">${item.postedAt}</div>
+              <div class="item__dot"></div>
+              <div class="item__contract">${item.contract}</div>
+              <div class="item__dot"></div>
+              <div class="item__location">${item.location}</div>
+            </div>
+          </div>
+          <div class="item__tag-wrapper text--cyan">
+            <div class="item__tag tag" data-role="${item.role}">${item.role}</div>
+            <div class="item__tag tag" data-level="${item.level}">${item.level}</div>
+            ${tagListHTML}
+          </div>
+      </div>
+      `)
+  })
+};
+
+function creatFilteredTagHtml(tag){
+    filteredTagWrapper.insertAdjacentHTML('afterbegin', `
+      <div class="filtered__tag tag">${tag}
+      <span class="filtered__remove-icon"><img src="images/icon-remove.svg" alt=""></span>
+      </div>
+    `)
+}
+
+
+
+
